742098af434ba9f6d8fb084f5f97a5489b659f62
Sebastian Murillo <sm021@uark.edu>
2017-08-14 20:30:02 -0500

    random emoji to the local server

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-function emojis() {
-  var emojiArray = [];
-  var emoji = require('node-emoji');
-   for ( var i = 0; i <= 20; i++ ) {
-    emojiArray.push(emoji.random().emoji);
-    console.log(emojiArray[i]);
-  };
-};
-emojis();
+// function emojis() {
+//   var emojiArray = [];
+//   var emoji = require('node-emoji');
+//    for ( var i = 0; i <= 20; i++ ) {
+//     emojiArray.push(emoji.random().emoji);
+//     console.log(emojiArray[i]);
+//   };
+// };
+// emojis();
+
+const http = require('http');
+const emoji = require('node-emoji');
+const server = http.createServer((req, res) => {
+  res.write(emoji.random().emoji);
+  res.end();
+});
+
+server.listen(8001);
